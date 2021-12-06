8e5610419347c58116430f5be3a9025ad03281ea
Maxim Komlik <maxim.komlik@gmail.com>
2021-12-06 19:22:59 +0300

    Compilation attempt
    Declaration order does not match definition order in structure.c and structure.h

--- structure.c
+++ structure.c
@@ -69,55 +69,54 @@
  */
 const struct Element PAD1 =
 {   //CB0
-    .inputBits = CBIMSEL_0,
-    .maxResponse = 250,
-    .threshold = 125
+    CBIMSEL_0,
+    125,
+    250
 };
 const struct Element PAD2 =
 {   //CB1
-    .inputBits = CBIMSEL_1,
-    .maxResponse = 390,
-    .threshold = 195
+    CBIMSEL_1,
+    195,
+    390
 };
 const struct Element PAD3 =
 {   //CB2
-    .inputBits = CBIMSEL_2,
-    .maxResponse = 340,
-    .threshold = 170
+    CBIMSEL_2,
+    170,
+    340
 };
 const struct Element PAD4 =
 {   //CB3
-    .inputBits = CBIMSEL_3,
-    .maxResponse = 500,
-    .threshold = 230
+    CBIMSEL_3,
+    230,
+    500
 };
 const struct Element PAD5 =
 {   //CB4
-    .inputBits = CBIMSEL_4,
-    .maxResponse = 400,
-    .threshold = 200
+    CBIMSEL_4,
+    200,
+    400
 };
 /*
  *  The sensor defines the grouping of elements, the method to measure change in
  *  capacitance, and the measurement time of each element.
  */
+
+struct Element* const _build_temp [MAXIMUM_NUMBER_OF_ELEMENTS_PER_SENSOR] = { &PAD1, &PAD2, &PAD3, &PAD4, &PAD5 };
+
 const struct Sensor keypad =
 {
-    .halDefinition = RO_COMPB_TA1_TA0,
-    .numElements = 5,
-    .baseOffset = 0,
-    .cbpdBits = 0x001F, //CB0,CB1,CB2,CB3,CB4
-    .arrayPtr[0] = &PAD1,
-    .arrayPtr[1] = &PAD2,
-    .arrayPtr[2] = &PAD3,
-    .arrayPtr[3] = &PAD4,
-    .arrayPtr[4] = &PAD5,
-    .cboutTAxDirRegister = (uint8_t *)&P1DIR,  // PxDIR
-    .cboutTAxSelRegister = (uint8_t *)&P1SEL,  // PxSEL
-    .cboutTAxBits = BIT6, // P1.6
+    RO_COMPB_TA1_TA0,
+    5,
+    0,
+    0x001F, //CB0,CB1,CB2,CB3,CB4
+    _build_temp,
+    (uint8_t *)&P1DIR,  // PxDIR
+    (uint8_t *)&P1SEL,  // PxSEL
+    BIT6, // P1.6
     // Timer Information
-    .measGateSource = TIMER_ACLK,      //  ACLK
-    .sourceScale = TIMER_SOURCE_DIV_0, // ACLK/1
+    TIMER_ACLK,      //  ACLK
+    TIMER_SOURCE_DIV_0, // ACLK/1
     /*  50 ACLK/1 cycles or 50*1/32Khz = 1.5ms  */
-    .accumulationCycles = 50
+    50
 };
